cc4deb7a06381749d0847643df718bde91c1537d
Robb Alexander <RobbMAlexander@gmail.com>
2022-01-25 14:21:58 -0800

    fixed remaining file name error

--- seed.js
+++ seed.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 mongoose.connect(process.env.DB_URL);
 
-const Books = require('./models/book');
+const Books = require('./models/books');
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error'));
 db.once('open', function () {
